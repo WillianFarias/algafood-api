f15d1abcc965fef0586fed34d0b8a9b23cb8645a
WillianFarias <williansousafarias@gmail.com>
2022-11-25 17:42:39 -0300

    Adicionando dois restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Comidas Tailandesas', 15);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Comidas Indianas', 10);
